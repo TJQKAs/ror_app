6500ff43eb4d0cb90c1d8fddd8d919e485cbc03d
TJQKAS <A26052013@GMAIL.COM>
2016-05-22 22:34:46 +0100

    front end

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'password_resets/new'
-
-  get 'password_resets/edit'
-
-      get 'account_activations/edit'
-      get 'sessions/new'
+  # get 'password_resets/new'
+  #
+  # get 'password_resets/edit'
+  #
+  #     get 'account_activations/edit'
+  #     get 'sessions/new'
 
       # get 'users/new'
 
@@ -16,8 +16,13 @@ Rails.application.routes.draw do
       get 'login' => 'sessions#new'
       post 'login' => 'sessions#create'
       delete 'logout' => 'sessions#destroy'
-      resources :users
+      resources :users do
+        member do
+          get :following, :followers
+        end
+      end
       resources :account_activations, only: [:edit]
       resources :password_resets, only: [:new, :create, :edit, :update]
-      resources :microposts, only:[:create, :destroy] 
+      resources :microposts, only:[:create, :destroy]
+      resources :relationships, only:[:create, :destroy]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-
+# User
 User.create!(
  name: "JohnnyBee",
  nickname: "Blink182",
@@ -16,7 +16,7 @@ User.create!(
  activated: true,
  activated_at: Time.zone.now )
 
-99.times do |n|
+399.times do |n|
   name = Faker::Name.name
   nickname = Faker::Name.name
   email = "example-#{n+2}@gambler.com"
@@ -31,8 +31,17 @@ User.create!(
    activated_at: Time.zone.now )
 end
 
+# Microposts
 users = User.order(:created_at).take(6)
-50.times do
+75.times do
   content = Faker::Lorem.sentence(5)
   users.each  {|user| user.microposts.create!(content: content)}
 end
+
+# Following relationships
+users = User.all
+user = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each {|followed| user.follow(followed)}
+followers.each {|follower| follower.follow(user)}
